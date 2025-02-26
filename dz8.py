780f275acca0e6c99e6b43d668301769fe49cdec
VoidRika <mycamilka20@gmail.com>
2025-02-26 20:02:13 +0400

    dz8

---
+++ dz8.py
@@ -0,0 +1,15 @@
+import unittest
+
+def Add(a, b):
+    return a * b
+
+class TestMathAdd(unittest.TestCase):
+    def test_multiply(self):
+        self.assertEqual(Add(2, 3), 6)
+        self.assertEqual(Add(-1, 5), -5)
+        self.assertEqual(Add(0, 100), 0)
+        self.assertEqual(Add(7, -2), -14)
+        self.assertEqual(Add(3.5, 2), 7.0)
+
+if __name__ == "__main__":
+    unittest.main()
